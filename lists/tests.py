30344e1a96e53e1de713e38ded00023c2607aac9
TomChen <fadeawaygod@gmail.com>
2018-07-22 17:08:06 +0800

    #5.2 Processing a POST Request on the Server

--- lists/tests.py
+++ lists/tests.py
@@ -4,9 +4,13 @@ from django.urls import resolve
 
 from lists.views import home_page
 
-
+#run 'python manage.py test' instead of run in VS Code.
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
